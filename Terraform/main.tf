5fbe5c827f822ce447ac50817b9d43de373deb33
SindhuManga <155552964+SindhuManga@users.noreply.github.com>
2025-11-17 11:53:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -32,7 +32,6 @@ resource "aws_iam_role_policy_attachment" "ssm_attach" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
 
